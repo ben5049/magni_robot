e5691daad3bbebdb9c4d2ee14d927a6deb38c034
Ben Smith <bens15351@gmail.com>
2025-03-16 23:13:22 +0000

    Tidied up positions graph

--- salmon/waypoints/visualise_waypoints.py
+++ salmon/waypoints/visualise_waypoints.py
@@ -1,7 +1,9 @@
 from json import load
 import graphviz
 
-waypoints_file = "positions.json"
+waypoints_file = (__file__.replace("\\", "/"))[::-1].split("/", 1)[1][::-1] + "/positions.json"
+
+print(f"Creating graph from file {waypoints_file}")
 
 waypoints = {}
 with open(waypoints_file,"r") as file:
